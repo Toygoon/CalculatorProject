d3b2ac34a4b8efabaea274e828cd969c5c2cac29
Toygoon <toygoon@gmail.com>
2021-12-12 22:49:20 +0900

    Basic calculator

--- src/main/java/com/toygoon/calculator/CalcController.java
+++ src/main/java/com/toygoon/calculator/CalcController.java
@@ -6,18 +6,12 @@ import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.util.Locale;
-import java.util.Stack;
-
 import static com.toygoon.calculator.CalculateNumbers.*;
 
 public class CalcController {
-    //private String inputText = "";
     private String operand = "";
     private String prevNum = "";
-    private String fullExpr = "";
     private boolean isOperating = false;
-    private boolean isPending = false;
     private boolean isDotPressed = false;
     private int listTop = 0;
 
@@ -36,7 +30,6 @@ public class CalcController {
         if(data.equals("CLEAR")) {
             operand = "";
             prevNum = "";
-            fullExpr = "";
             if(isOperating) {
                 list_history.getItems().remove(listTop);
             }
@@ -84,66 +77,5 @@ public class CalcController {
             field_calc.setText(field_calc.getText() + data);
         }
         list_history.scrollTo(listTop);
-    /*
-        if(data.equals("CLEAR")) {
-            field_calc.setText("");
-            prevNum = "";
-            operand = "";
-            if(!(list_history.getItems().get(listTop).equals("")))
-                list_history.getItems().remove(listTop--);
-        } else if(isOperands(data)) {
-            if(isOperating) {
-                switch(operand) {
-
-                }
-            } else {
-                prevNum = field_calc.getText();
-                operand = data;
-                field_calc.setText("");
-                list_history.getItems().add(listTop, prevNum + " " + getOperands(operand));
-                isOperating = true;
-            }
-        } else {
-            field_calc.setText(field_calc.getText() + data);
-        }
-        */
-        /*
-        if(data.equals("CLEAR")) {
-            inputText = "";
-            operand = "";
-            prevNum = "";
-            fullExpr = "";
-            if(isOperating)
-                list_history.getItems().remove(listTop);
-            isOperating = false;
-        } else if (isOperands(data)) {
-            fullExpr += inputText + " " + getOperands(data) + " ";
-            if(isOperating) {
-                list_history.getItems().remove(listTop);
-                inputText = calcResults(prevNum, inputText, operand);
-                fullExpr += "= " + inputText;
-                isDone = true;
-                isOperating = false;
-            } else {
-                isOperating = true;
-                operand = data;
-                prevNum = inputText;
-                inputText = "";
-            }
-            if(!(fullExpr.equals("")))
-                list_history.getItems().add(listTop, fullExpr);
-        } else {
-            inputText += data;
-        }
-
-        if(isDone) {
-            fullExpr = "";
-            listTop++;
-            isDone = false;
-        }
-        list_history.scrollTo(list_history.getItems().size());
-        field_calc.setText(inputText);
-
-         */
     }
 }
